775329485409bea3ab13e72a6db9266ae282aefe
MrJake222 <norbertzpilicy@gmail.com>
2024-06-04 23:20:55 +0200

    Added my variant

---
+++ src/main/scala/vexriscv/demo/GenSmallAndProductiveMy.scala
@@ -0,0 +1,81 @@
+package vexriscv.demo
+
+import vexriscv.plugin._
+import vexriscv.{plugin, VexRiscv, VexRiscvConfig}
+import spinal.core._
+
+/**
+ * Created by spinalvm & mrjake222
+ */
+object GenSmallAndProductiveMy extends App{
+  def smallest_my(mtvecInit : BigInt)  = CsrPluginConfig(
+    catchIllegalAccess = false,
+    mvendorid      = null,
+    marchid        = null,
+    mimpid         = null,
+    mhartid        = null,
+    misaExtensionsInit = 66,
+    misaAccess     = CsrAccess.NONE,
+    mtvecAccess    = CsrAccess.NONE,
+    mtvecInit      = mtvecInit,
+    mepcAccess     = CsrAccess.NONE,
+    mscratchGen    = false,
+    mcauseAccess   = CsrAccess.READ_ONLY,
+    mbadaddrAccess = CsrAccess.NONE,
+    mcycleAccess   = CsrAccess.READ_ONLY,
+    minstretAccess = CsrAccess.READ_ONLY,
+    ecallGen       = false,
+    wfiGenAsWait         = false,
+    ucycleAccess   = CsrAccess.NONE,
+    uinstretAccess = CsrAccess.NONE
+  )
+    
+  def cpu() = new VexRiscv(
+    config = VexRiscvConfig(
+      plugins = List(
+        new IBusSimplePlugin(
+          resetVector = 0x00020000l,
+          cmdForkOnSecondStage = false,
+          cmdForkPersistence = true,
+          prediction = NONE,
+          catchAccessFault = false,
+          compressedGen = false
+        ),
+        new DBusSimplePlugin(
+          catchAddressMisaligned = false,
+          catchAccessFault = false
+        ),
+        new CsrPlugin(smallest_my(0x00000020l)),
+        new DecoderSimplePlugin(
+          catchIllegalInstruction = false
+        ),
+        new RegFilePlugin(
+          regFileReadyKind = plugin.SYNC,
+          zeroBoot = false
+        ),
+        new IntAluPlugin,
+        new SrcPlugin(
+          separatedAddSub = false,
+          executeInsertion = true
+        ),
+        new LightShifterPlugin,
+        new HazardSimplePlugin(
+          bypassExecute           = true,
+          bypassMemory            = true,
+          bypassWriteBack         = true,
+          bypassWriteBackBuffer   = true,
+          pessimisticUseSrc       = false,
+          pessimisticWriteRegFile = false,
+          pessimisticAddressMatch = false
+        ),
+        new BranchPlugin(
+          earlyBranch = false,
+          catchAddressMisaligned = false
+        ),
+        new YamlPlugin("cpu0.yaml")
+      )
+    )
+  )
+
+  SpinalVerilog(cpu())
+}
